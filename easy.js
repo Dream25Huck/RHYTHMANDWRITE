74a1138284a1c9122c2401b0bbaefe43ce99432d
Adil <142867662+Dream25Huck@users.noreply.github.com>
2024-05-10 04:43:03 +0500

    easy.js

--- easy.js
+++ easy.js
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const span = document.querySelectorAll('.span');
     let updateInterval;
     const seekBar = document.querySelector('.seek-bar');
-
+    const bkgEasyContainer = document.querySelector('.easyContainer');
+    const images = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
     const titles = ['Happy', "Can't Stop The Feeling!", "Don't Worry, Be Happy", "Over the Rainbow", 'Count On Me', "Happier", "Here Comes The Sun"];
     const beginnerArtists = [
         "Pharrell Williams",
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     li.forEach((element, index) => {
         element.addEventListener('click', () => {
             clearInterval(updateInterval); // Clear any previous interval
-            
+            bkgEasyContainer.style.backgroundImage = `url(img/${images[index]}.png)`;    
             
             
             audio.src = `music/${index+1}.mp3`; // Set new source for audio
